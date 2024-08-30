fe58661c74a4fc66d533f8e55e08660eac6d38d8
Leandro Serra <coquiserra1992@gmail.com>
2024-08-29 23:22:34 -0300

    Make components and add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Form from './components/Form';
+import Modal from './components/Modal';
+import Preview from './components/Preview';
+import './css/app.css';
+
 function App() {
 	return (
 		<>
 			<h1>cv maker</h1>
+			<div id='cv-container'>
+				<Form />
+				<Preview />
+			</div>
+			<Modal />
 		</>
 	);
 }
---
+++ src/components/Preview.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Preview() {
+	return <main className='preview-container'>ACA MOSTRAR EL CV ACTUALIZANDOSE</main>;
+}
+
+export default Preview;
